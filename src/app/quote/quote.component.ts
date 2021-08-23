b420fb8542cd029520125aec3e7dc18513d578f9
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-08-23 04:16:40 +0300

    created some quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  // quotes:Quote[]=[
-  //   new Quote (1, 'All of my friends who have younger siblings who are going to college or high school - my number one piece of advice is: You should learn how to program.', 'Mark Zuckerberg', 'Lawi Otieno', new Date(2021,8,20)),
-  //   new Quote (1, 'Coding is the language of the future, and every girl should learn it. As I have learned from watching girls grow and learn in our classrooms, coding is fun, collaborative and creative.', 'Reshma Saujani', 'Kingi Elijah', new Date(2021,8,22)),
-  // ];
+  quotes:Quote[]=[
+    new Quote (1, 'All of my friends who have younger siblings who are going to college or high school - my number one piece of advice is: You should learn how to program.', 'Mark Zuckerberg', 'Lawi Otieno', new Date(2021,8,20)),
+    new Quote (1, 'Coding is the language of the future, and every girl should learn it. As I have learned from watching girls grow and learn in our classrooms, coding is fun, collaborative and creative.', 'Reshma Saujani', 'Kingi Elijah', new Date(2021,8,22)),
+  ];
 
   // toggleDetails(index){
   //   this.quotes[index].showQuotes = !this.quotes[index].showQuotes
